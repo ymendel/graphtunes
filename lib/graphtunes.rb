0f4a08cf8100f36d50ddf75aa67a9073c85cb0d1
Yossef Mendelssohn <ymendel@pobox.com>
2008-05-02 20:57:18 -0500

    Start of data-importing behavior

--- lib/graphtunes.rb
+++ lib/graphtunes.rb
@@ -2,5 +2,10 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Graphtunes
-  
+  class << self
+    def import(file)
+      raise TypeError, "'#{file}' is not a file" unless File.file?(file)
+      File.read(file)
+    end
+  end
 end
--- spec/graphtunes_spec.rb
+++ spec/graphtunes_spec.rb
@@ -1,11 +1,37 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
-describe "Place your specs here" do
-  
-  it "find this spec in spec directory" do
-    violated "Be sure to write your specs"
+describe Graphtunes do
+  it 'should import data' do
+    Graphtunes.should respond_to(:import)
   end
   
+  describe 'importing data' do
+    before :each do
+      @filename = 'blah'
+      File.stubs(:file?).returns(true)
+    end
+    
+    it 'should require a filename' do
+      lambda { Graphtunes.import }.should raise_error(ArgumentError)
+    end
+    
+    it 'should accept a filename' do
+      lambda { Graphtunes.import(@filename) }.should_not raise_error(ArgumentError)
+    end
+    
+    it 'should require the argument to be a real file' do
+      File.stubs(:file?).with(@filename).returns(false)
+      lambda { Graphtunes.import(@filename) }.should raise_error(TypeError)
+    end
+    
+    it 'should require the argument to be a real file' do
+      File.stubs(:file?).with(@filename).returns(true)
+      lambda { Graphtunes.import(@filename) }.should_not raise_error(TypeError)
+    end
+    
+    it 'should read the contents of the file' do
+      File.expects(:read).with(@filename)
+      Graphtunes.import(@filename)
+    end
+  end
 end
