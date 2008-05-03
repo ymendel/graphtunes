a727d9d6701945164971ea0d9f2caf9585fa8b36
Yossef Mendelssohn <ymendel@pobox.com>
2008-05-02 22:17:10 -0500

    Fleshed out the data-importing behavior and getting the tracks.

--- lib/graphtunes.rb
+++ lib/graphtunes.rb
@@ -1,11 +1,28 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'rexml/document'
+
 module Graphtunes
   class << self
     def import(file)
       raise TypeError, "'#{file}' is not a file" unless File.file?(file)
-      File.read(file)
+      get_tracks(File.read(file))
+    end
+    
+    def get_tracks(data)
+      doc = REXML::Document.new(data)
+      
+      tracks = nil
+      doc.elements.each('plist/dict/key') do |el|
+        if el.text == 'Tracks'
+          tracks = el.next_element
+        end
+      end
+      
+      raise 'No track information found' unless tracks
+      
+      get_track_list(tracks)
     end
   end
 end
--- spec/graphtunes_spec.rb
+++ spec/graphtunes_spec.rb
@@ -9,6 +9,9 @@ describe Graphtunes do
     before :each do
       @filename = 'blah'
       File.stubs(:file?).returns(true)
+      @data = stub('file data')
+      File.stubs(:read).with(@filename).returns(@data)
+      Graphtunes.stubs(:get_tracks)
     end
     
     it 'should require a filename' do
@@ -32,6 +35,87 @@ describe Graphtunes do
     it 'should read the contents of the file' do
       File.expects(:read).with(@filename)
       Graphtunes.import(@filename)
+    end
+    
+    it 'should get the track information from the file' do
+      Graphtunes.expects(:get_tracks).with(@data)
+      Graphtunes.import(@filename)
+    end
+  end
+  
+  it 'should get track information' do
+    Graphtunes.should respond_to(:get_tracks)
+  end
+  
+  describe 'getting track information' do
+    before :each do
+      @data = %q[
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+          <key>Tracks</key>
+      	  <dict>
+          </dict>
+        </dict>
+        </plist>
+      ]
+      Graphtunes.stubs(:get_track_list)
+    end
+    
+    it 'should require data' do
+      lambda { Graphtunes.get_tracks }.should raise_error(ArgumentError)
+    end
+    
+    it 'should accept data' do
+      lambda { Graphtunes.get_tracks(@data) }.should_not raise_error(ArgumentError)
+    end
+    
+    it 'should require there to be a notifier of track data' do
+      @data = %q[
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+          <key>Major Version</key><integer>1</integer>
+        </dict>
+        </plist>
+      ]
+      lambda { Graphtunes.get_tracks(@data) }.should raise_error
+    end
+    
+    it 'should require there to be track data' do
+      @data = %q[
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+          <key>Tracks</key>
+        </dict>
+        </plist>
+      ]
+      lambda { Graphtunes.get_tracks(@data) }.should raise_error
+    end
+    
+    
+    it 'should accept data that includes track data' do
+      @data = %q[
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+          <key>Tracks</key>
+      	  <dict>
+          </dict>
+        </dict>
+        </plist>
+      ]
+      lambda { Graphtunes.get_tracks(@data) }.should_not raise_error
+    end
+    
+    it 'should get the track list' do
+      Graphtunes.expects(:get_track_list)
+      Graphtunes.get_tracks(@data)
     end
   end
 end
